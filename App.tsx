4ca5cb93bffa376f637e988a35779476cf9de625
junguksim <simju9397@gmail.com>
2022-07-21 14:30:19 +0900

    Add asking camera permission

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React, {useEffect} from 'react';
 import {Alert, BackHandler, SafeAreaView, StyleSheet, View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-
+import {PERMISSIONS, request, check, RESULTS} from 'react-native-permissions';
 import SlidWebView from './components/SlidWebView';
 
 const App = () => {
   useEffect(() => {
+    check(PERMISSIONS.IOS.CAMERA).then(permissionResult => {
+      if (permissionResult !== RESULTS.GRANTED) request(PERMISSIONS.IOS.CAMERA);
+    });
     setTimeout(() => {
       SplashScreen?.hide();
     }, 500);
